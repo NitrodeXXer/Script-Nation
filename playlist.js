90df5c09d6c72b4ec20a7f6d8bd5d62bad6a7634
NitrodeXXer <CrazyCrafters@gmx.de>
2016-01-04 09:51:03 +0100

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,27 @@
+function playlist() {
+var g=0,h=0;
+//Hier kommen die ganzen Videos her!
+var d=new Array (
+"Script Nation Video",
+"Itro & Tobu - Cloud 9 [NCS Release]",
+"Alan Walker - Spectre [NCS Release]",
+"Different Heaven - OMG [NCS Release]",
+"Jim Yosef - Arrow [NCS Release]",
+"Tobu - Colors [NCS Release]",
+"Alan Walker - Force [NCS Release]",
+"Itro - Skyward Bound (feat. Kédo Rebelle) [NCS Release]",
+"Tobu - Candyland [NCS Release]",
+"Tobu - Sound of Goodbye [NCS Release]",
+"Alan Walker - Fade [NCS Release]",
+"Ahrix - Nova [NCS Release]",
+"JPB - Defeat The Night (feat. Ashley Apollodor) [NCS Release]",
+"Kygo - Here For You ft. Ella Henderson (Fabian Olander Remix)",
+"023 Launchpad Freestyle",
+"tutorial",
+"customvideo",//Wenn du eigene Videos hinzufügen willst, füge
+"R3hab NERVO Ummet Ozcan - Revolution (Official Video HD)"//HIER hinter dem " ein Komma ein.Mache dann einen Neue Zeile und schreibe dort in Anführungszeichen den Namen des neuen Videos.
+);
+//WICHTIG!!!!! Die Videos müssen immer durch ein Komma getrennt werden, nur ganz aim Ende darf keins stehen!
+//z.B: ("Script Nation Video","Peters Vlog","Lets Play Roulette #001")
+return d;
+}
